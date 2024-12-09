3dbaa620c0c9805af40f63120799be098cffba22
Cathz18 <cathy16bantilan@gmail.com>
2024-12-09 23:45:06 +0800

    added vuetify

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -10,7 +10,7 @@ const { mobile } = useDisplay()
 <template>
   <AppLayout>
     <template #content>
-      <v-row class="d-flex justify-center mt-5">n
+      <v-row class="d-flex justify-center mt-5">
         <v-col cols="12" md="6" class="mx-auto">
           <v-card class="mx-auto" color="grey-lighten-1" rounded="lg" elevation="8">
             <template v-slot:title>
